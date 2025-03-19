47db539f1e4b6a4feef0ffd0920c992785e715ca
PedroCasacchi1 <pedro.casacchi@edu.unifil.br>
2025-03-19 15:20:12 -0300

    Adicionando backend e mysql no Docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ routes(app);
 // Inicia o servidor
 const start = async () => {
   try {
-    await app.listen(3000);
+    await app.listen({ port: 3000, host: "0.0.0.0" });
     console.log("Server is running at http://localhost:3000");
   } catch (err) {
     app.log.error(err);
