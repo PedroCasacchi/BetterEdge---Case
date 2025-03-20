5f963f63e995b9f756aadd9a287b52f7161d3548
PedroCasacchi1 <pedro.casacchi@edu.unifil.br>
2025-03-20 15:31:03 -0300

    Correções

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,20 @@ import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
 import routes from "./routes/routes"; // Importa as rotas
 import errorHandler from "./middlewares/errorHandler"; // (Opcional) Lidar com erros de forma centralizada
+import fastifyCors from "fastify-cors";
 
 const prisma = new PrismaClient(); // Instancia o cliente Prisma
-const app = Fastify();
+const app = Fastify(); // Instância correta do Fastify
 
 // Middleware para lidar com erros
 app.setErrorHandler(errorHandler);
+
+// Registrando o CORS corretamente na instância "app"
+app.register(fastifyCors, {
+  // Configurações do CORS (opcional)
+  origin: "*", // Permite qualquer origem, você pode personalizar isso
+  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+});
 
 // Registra as rotas
 routes(app);
